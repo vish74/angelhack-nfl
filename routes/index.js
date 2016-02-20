867da95764c25e539a5ca32f5a0da277b61c1cd3
Vishvesh Shah <vmshah@scu.edu>
2016-02-20 15:27:32 -0800

    added route

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,27 @@
+const fs = require('fs');
+
+function readGame(gameIndex) {
+    return fs.readFileSync('data/full-game-1.json');
+}
+
+function parseGame(gameIndex) {
+    return readGame(gameIndex).toString().trim().split('\n').map(JSON.parse);
+}
+
+function readTeam(teamIndex) {
+    return fs.readFileSync('data/team1.json');
+}
+
+function parseTeam(teamIndex) {
+    return JSON.parse(readGame(teamIndex));
+}
+
 exports.first = function (req, res) {
-    res.render('index');
+    var game = parseGame(1);
+
+    var play = game[2];
+    var aa = JSON.stringify(play);
+
+    res.render('index', {'title': aa});
+
 };
