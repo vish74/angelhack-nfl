badeafe382d65227feb8c46f8e8a6ea2b4892a9d
Ian Macalinao <me@ian.pw>
2016-02-20 16:57:27 -0800

    fix: update readme

--- script/train.js
+++ script/train.js
@@ -8,7 +8,6 @@ const game = trainer.Game.fromIndex(1, players);
 const play = game.getPlay(2);
 
 console.log(play.play.playStats);
-console.log(play.homeTrackingData[0])
 
 /*
 console.log(play.homeTrackingData[0].map(function(data) {
